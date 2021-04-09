1ef1fe66fc3018e5665a6e31e15431673ea00b2f
rnuesse <ralph_nuesse@bmc.com>
2021-04-09 12:20:21 +0200

    Test

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
@@ -7,7 +7,6 @@
       *  THIS CALLED PROGRAM IS PART OF THE COBOL DEMO PROGRAM USED    *
       *  FOR XPEDITER/TSO TRAINING SESSIONS                            *
       *                                                                *  
-      *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
