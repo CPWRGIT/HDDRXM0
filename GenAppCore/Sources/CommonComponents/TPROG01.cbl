eee32e2f18a917dab0e044c9be24d7f77802d241
Ralph Nuesse <ralph_nuesse@bmc.com>
2021-04-22 13:20:48 +0200

    Run with non existing scenarios

--- GenAppCore/Sources/CommonComponents/TPROG01.cbl
+++ GenAppCore/Sources/CommonComponents/TPROG01.cbl
@@ -11,7 +11,6 @@
 001100*      AND COPYLIBS   TCPYA01   (HEADER)
 001200*                     TCPYB01   (LINK AREA)
 001300* ======================================================
-001100* ======================================================
 001400*  ISPW (TM)
 001500*  (C) COPYRIGHT 1996, 2004  BENCHMARK TECHNOLOGIES LTD.
 001600*  THIS PRODUCT CONTAINS PROPRIETARY MATERIAL, AND MAY
